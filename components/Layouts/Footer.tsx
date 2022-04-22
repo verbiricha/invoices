33349714c1ad3a38eee816aad0cb47336b86425c
Alejandro Gomez <alejandro@strike.me>
2022-04-22 11:55:20 +0200

    Strike tip link

--- components/Layouts/Footer.tsx
+++ components/Layouts/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <Flex height="50px" width="100%" justifyContent="center">
       <Text textAlign="center" color="gray.500" fontSize="sm">
-        Made with ❤️  and the <Link color="brand.500" isExternal href="https://developer.strike.me/">Strike API</Link> by <Link color="brand.500" isExternal href="https://strike.me/manuelarios/">Manuela</Link> and <Link color="brand.500" isExternal href="https://lntxbot.com/@verbiricha">Alejandro</Link>
+        Made with ❤️  and the <Link color="brand.500" isExternal href="https://developer.strike.me/">Strike API</Link> by <Link color="brand.500" isExternal href="https://strike.me/manuelarios">Manuela</Link> and <Link color="brand.500" isExternal href="https://strike.me/verbiricha">Alejandro</Link>
       </Text>
     </Flex>
   )
